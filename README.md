60210b60a3459fca6ef881a653091f6b81316df4
Boyu Zhao <b.zhao1@tue.nl>
2026-03-05 15:40:16 +0100

    workspace structure build with offline mode coding

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # AD-fusionbox
-A sensors - vehicle data fusion box
+A sensors - vehicle data fusion box based on ROS2 Humble
+
+Computers - Heterogeneous computing
+- Perception Layer: Jetson AGX Orin 64 GB
+- Decision Layer: ARK 2251
+
+Sensors:
+- 4 $\times$ Continental SRR3-A Short Range Radar,
+- 4 $\times$ ZED X Stereo Cameras,
+- 1 $\times$ Continental SC13S 6-DoF IMU,
+- 2 $\times$ RTK GNSS
+
+Vechicle:
+- Nissan LEAF MY2012
---
+++ src/drivers/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+package_name = 'streetlive_drivers'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='jet_orin',
+    maintainer_email='jet_orin@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
