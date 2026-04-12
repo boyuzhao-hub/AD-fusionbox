489f1d0c363df58fa160944959e5d829bfcacebe
Boyu Zhao <b.zhao1@tue.nl>
2026-04-12 15:47:23 +0200

    [Coding] add urdf and relatvie robot_state_publisher for frames management

---
+++ src/bringup/launch/rsp_launch.py
@@ -0,0 +1,26 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+import xacro
+
+def generate_launch_description():
+    # Find the path to the Xacro file
+    pkg_name = 'bringup'
+    xacro_file = os.path.join(get_package_share_directory(pkg_name), 'urdf', 'box.urdf.xacro')
+
+    # Process the Xacro file to get the robot description
+    robot_description_config = xacro.process_file(xacro_file)
+    params = {'robot_description': robot_description_config.toxml()}
+
+    # Launch the robot_state_publisher node with the robot description
+    node_robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[params]
+    )
+
+    return LaunchDescription([
+        node_robot_state_publisher
+    ])
